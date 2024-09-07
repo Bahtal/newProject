1a6e7da1c76946a22629bec08139e7ecea10aaf8
Bahtal <bahtal777artem@gmail.com>
2024-09-07 21:46:32 +0300

    With Func(null)

--- src/components/movies/ShowMovies.js
+++ src/components/movies/ShowMovies.js
@@ -2,15 +2,23 @@
 
 function ShowMovies(data) {
 	const moviesEl = document.querySelector(".movies");
+	
 
 	data.items.forEach((movie) => {
+		const Rating = function() {
+			if(movie.ratingImdb == null) {
+				return 0;
+			} else {
+				return movie.ratingImdb;
+			}
+		};
 		const movieEl = document.createElement("div")
 		moviesEl.classList.add("movie")
 		movieEl.innerHTML = `
 			<div class="movie__background">
 				<div class="top--image">
 						<img class="movie__img" src="${movie.posterUrlPreview}" alt="${movie.nameOriginal}"/>
-						<div class="movie__average">${movie.ratingImdb}</div>
+						<div class="movie__average">${Rating()}</div>
 						</div>
 						<div class="movie__info">
 						<div class="movie__title">${movie.nameRu}</div>
